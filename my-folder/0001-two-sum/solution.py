23c487159226737a8d9b2fa8c401ad1715c2e74e
Julian Ngong <62028472+julianngong@users.noreply.github.com>
2023-10-05 19:54:27 +0000

    [LeetCode Sync] Runtime - 69 ms (33.16%), Memory - 17.5 MB (100.00%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -1,14 +1,32 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        output = []
-        for i in range(len(nums)-1):
-            tempnums = nums[:i] + nums[i+1 :]
-            if (target-nums[i]) in tempnums:
-                if ((i not in output) and (nums.index(target-nums[i]) not in output)):
-                    output.append(i)
-                    temp = nums[i]
-                    nums[i] = 'a'
-                    output.append(nums.index(target-temp))
-                    nums[i] = temp
-        return(output)
-        
+class Solution:
+    def twoSum2(self, nums: List[int], target: int) -> List[int]:
+        #[i for i in range(len(nums))] makes a list of indices from 0 to len(nums)-1
+        #list(zip(list1,list2)) makes a list of tuples
+        #sorted_list = sorted(tuple_list) will sort the tuple list based on the first element
+        #sorted_list = sorted(tuple_list, key=lambda x: x[1]) sorts it by the second element
+        numandindex = list(zip(nums, [i for i in range(len(nums))]))
+        nums = sorted(numandindex)
+        front = len(nums)-1
+        back = 0
+        found = False
+        while found == False:
+            added = nums[back][0]+nums[front][0]
+            print("sum :",added)
+            if added > target:
+                front-=1
+            elif added < target:
+                back+=1
+            else:
+                found = True
+        return([nums[back][1],nums[front][1]])
+    
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        table = {}
+        for i in range(len(nums)):
+            table[nums[i]]=i
+        print(table)
+        for j in range(len(nums)):
+            remainder = target - nums[j]
+            if remainder in table.keys() and table[remainder] != j:
+                return([j,table[remainder]])
+            
