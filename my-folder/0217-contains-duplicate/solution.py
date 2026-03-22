4c22882a99ff1ba4d554fd9f78f16ae609abb097
Julian Ngong <62028472+julianngong@users.noreply.github.com>
2026-03-22 23:55:24 +0000

    [LeetCode Sync] Runtime - 27 ms (15.71%), Memory - 36.1 MB (18.66%)

--- my-folder/0217-contains-duplicate/solution.py
+++ my-folder/0217-contains-duplicate/solution.py
@@ -1,14 +1,17 @@
+from collections import defaultdict
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        result = False
-        seen = dict({})
-        i=0
-        while result == False and i < len(nums):
-            if nums[i] not in seen.keys():
-                seen[nums[i]] = 1
-            else:
-                result = True
-            i += 1
-        return(result)
+        uniques = defaultdict(bool)
+        for num in nums:
+            if uniques[num] == 1:
+                return True
+            uniques[num] = 1
+        return False
+    
+    def containsDuplicateOptimal(self, nums: List[int]) -> bool:
+        return len(set(nums)) < len(nums)
+        
+# Note: For things to do with duplicates try thinking about using the set to find the unique ones and then see if theres any comparisons which could be done with the number of elements in the set vs the original list.
 
-#The big difference here is using a dicitonary to check if something saved has been seen before as accessing something from list is O(n^2) while for a dictionary is O(n)
+# Note: Think about using a set here as its much more memory efficient so can just make an empty set then add the values to the empty set then see if its inside the set.
