558a2750c1b539b27c11cf37a050392346189989
Julian Ngong <62028472+julianngong@users.noreply.github.com>
2026-03-23 00:11:35 +0000

    [LeetCode Sync] Runtime - 12 ms (54.37%), Memory - 19.4 MB (65.52%)

--- my-folder/0242-valid-anagram/solution.py
+++ my-folder/0242-valid-anagram/solution.py
@@ -1,24 +1,22 @@
+from collections import defaultdict
+
 class Solution:
-    def isAnagram2(self, s: str, t: str) -> bool:
-        ssort = ''.join(sorted(s))
-        tsort = ''.join(sorted(t))
-        if ssort == tsort:
-            return(True)
-        else:
-            return(False)
-
     def isAnagram(self, s: str, t: str) -> bool:
-        count = defaultdict(int)
-        print(count)
-        for letter in s:
-            count[letter] += 1
-        for letter in t:
-            count[letter] -= 1
-        for num in count.values():
-            if num !=0:
-                return(False)
-        return(True)
+        letterCount = defaultdict(int)
+        for sletter in s:
+            letterCount[sletter] += 1
+        for tletter in t:
+            letterCount[tletter] -= 1
+            if letterCount[tletter] < 0:
+                return False
+        if set(letterCount.values()) == {0}:
+            return True
+        return False
 
-# Default dict is best to use instead of dict as it handles when you dont already have the key value pair there. if you dont have a key set and you request to access a key it will make a new key there and default its value to 0 if the int parameter is given. if the list parameter is given it will default it to an empty list
-
+# Good solution, can't see anything wrong with it and a good early break case for when it goes negative. Can do it in one pass tho using enumerate. unemerate through s or t as they have to have the same length to be correct then decrement each one and do the same checking you did.
+'''
+for i,_ in enumerate(s):
+            cnt[s[i]]+=1
+            cnt[t[i]]-=1
+'''
         
