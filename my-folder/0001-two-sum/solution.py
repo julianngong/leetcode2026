56176ef95f315a370b3c486bd687c7eefeab25a8
Julian Ngong <62028472+julianngong@users.noreply.github.com>
2026-03-23 00:40:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.5 MB (54.56%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -1,32 +1,28 @@
 class Solution:
-    def twoSum2(self, nums: List[int], target: int) -> List[int]:
-        #[i for i in range(len(nums))] makes a list of indices from 0 to len(nums)-1
-        #list(zip(list1,list2)) makes a list of tuples
-        #sorted_list = sorted(tuple_list) will sort the tuple list based on the first element
-        #sorted_list = sorted(tuple_list, key=lambda x: x[1]) sorts it by the second element
-        numandindex = list(zip(nums, [i for i in range(len(nums))]))
-        nums = sorted(numandindex)
-        front = len(nums)-1
-        back = 0
-        found = False
-        while found == False:
-            added = nums[back][0]+nums[front][0]
-            print("sum :",added)
-            if added > target:
-                front-=1
-            elif added < target:
-                back+=1
+    def twoSumFail(self, nums: List[int], target: int) -> List[int]:
+        i,j = 0,1
+        numlen = len(nums)
+        while i<numlen:
+            total = nums[i] + nums[j]
+            if total == target:
+                return [i,j]
             else:
-                found = True
-        return([nums[back][1],nums[front][1]])
-    
+                if i < j-1 or j == numlen - 1:
+                    i += 1
+                else:
+                    j += 1
+
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        table = {}
-        for i in range(len(nums)):
-            table[nums[i]]=i
-        print(table)
-        for j in range(len(nums)):
-            remainder = target - nums[j]
-            if remainder in table.keys() and table[remainder] != j:
-                return([j,table[remainder]])
-            
+        seenMap = {}
+        firstNumIndex = 0
+        for i, val in enumerate(nums):
+            remainder = target - val
+            if remainder in seenMap:
+                return([i, seenMap[remainder]])
+            else:
+                seenMap[val] = i
+
+
+'''
+they mention if its o(n^2) and space o(1) then its likely it can be changed to o(n) time and space
+'''
