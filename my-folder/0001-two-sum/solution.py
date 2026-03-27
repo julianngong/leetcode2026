1a450ba2c0fac84f399d54acbe1ec556f507a0f5
Julian Ngong <62028472+julianngong@users.noreply.github.com>
2026-03-27 23:40:05 +0000

    [LeetCode Sync] Runtime - 3 ms (53.02%), Memory - 20.5 MB (52.88%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -21,8 +21,22 @@ class Solution:
                 return([i, seenMap[remainder]])
             else:
                 seenMap[val] = i
+    '''
+    they mention if its o(n^2) and space o(1) then its likely it can be changed to o(n) time and space. We will look at the remainder and see if the remainder has been previously seen in the history but the important thing is we need to keep track which numbers weve seen as we go through and which index its at.
+    '''
 
+    def twoSumSecondAttempt(self, nums: List[int], target: int) -> List[int]:
+        num_to_index = {}
+        res = []
+        for i in range(len(nums)):
+            num_to_index[nums[i]] = i
+        for j in range(len(nums)):
+            remainder = target - nums[j]
+            if remainder in num_to_index and num_to_index[remainder] != j:
+                res.append(min(j,num_to_index[remainder]))
+                res.append(max(j,num_to_index[remainder]))
+                return res
 
-'''
-they mention if its o(n^2) and space o(1) then its likely it can be changed to o(n) time and space
-'''
+    '''
+    This was second try and was good because you did the seen map. note we dont need to iterate once to populate and then iterate again. you can just populate in the same pass because although it wont be picked up at the start, by the time you're at the end the thing you need to add would be in the list
+    '''
