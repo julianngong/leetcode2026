e05eeff33d243173ae71baae1a9362533788c6e4
Julian Ngong <62028472+julianngong@users.noreply.github.com>
2026-04-10 01:21:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (36.55%)

---
+++ my-folder/0226-invert-binary-tree/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return root
+        
+        left = self.invertTree(root.left)
+        right = self.invertTree(root.right)
+        root.left = right
+        root.right = left
+        return root
