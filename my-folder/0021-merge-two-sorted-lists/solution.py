11add7251ac28bff164fa5cc4d5631349e080286
Julian Ngong <62028472+julianngong@users.noreply.github.com>
2026-04-10 01:02:55 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.4 MB (48.00%)

--- my-folder/0021-merge-two-sorted-lists/solution.py
+++ my-folder/0021-merge-two-sorted-lists/solution.py
@@ -60,3 +60,19 @@ class Solution:
         # Return the actual start of the list, ignoring the blank dummy node
         return dummy.next
         
+    def mergeTwoListsTry2(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy_node = ListNode()
+        curr = dummy_node
+        while list1 and list2:
+            if list1.val < list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+        if list1:
+            curr.next = list1
+        if list2:
+            curr.next = list2
+        return dummy_node.next
